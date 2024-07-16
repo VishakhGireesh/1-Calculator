a0d234a45d191091b9ba60d17be53667de1a1bc5
vishakh <vishakhhrithi007@gmail.com>
2024-07-16 11:47:18 +0530

    if added

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let result = document.getElementById("result");
-result.addEventListener("click", results);
+var full = document.getElementById("X");
+let Door = document.getElementById("Y");
+result.addEventListener("click",validateForm);
+function validateForm(){
+    let ful = full.value;
+    let Doorz = Door.value;
+if(ful==""||ful==null||Doorz==""||null){    
+
+    return false;
+}else{
+    results()
+}
+}
 function results() {
-    let full = document.getElementById("X").value;
-    const Door = document.getElementById("Y").value;
-    let minus = Door - 1;
+    let ful = full.value;
+    let Doorz = Door.value;
+    let minus = Doorz - 1;
     let answer = minus * 3.2;
-    let result=full-answer;
-    const total=result/Door
-    if(result==true){
-    var grandresult=document.getElementById("ans");
-    }
-    FR= (truncator(total,4));
+    let result = ful - answer;
+    var total = result / Doorz;
+    var grandresult = document.getElementById("ans");
+
+    let FR = truncator(total, 4);
+
+    grandresult.innerHTML = FR + "cm";
     
-grandresult.innerHTML=FR+"cm";
+
 }
 
-function truncator(numToTruncate, intDecimalPlaces) {    
-var numPower = Math.round(100, intDecimalPlaces); // "numPowerConverter" might be better
-return ~~(numToTruncate * numPower)/numPower;
+
+function truncator(numToTruncate, intDecimalPlaces) {
+    var numPower = Math.round(100, intDecimalPlaces); // "numPowerConverter" might be better
+    return ~~(numToTruncate * numPower) / numPower;
 }
+
+
